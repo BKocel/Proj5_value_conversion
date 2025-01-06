e6db0b0a67f025565ef106ed8e84adcec9d0ea04
Bartosz Kocel <bartosz.kocel@outlook.com>
2025-01-06 21:11:24 +0100

    Weightcalc working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # operating modes
 def weightcalc(): # weight calculations
-    value = float(input("Podaj wartość do przekonwertowania"))
+    value = float(input("Podaj wartość do przekonwertowania: "))
     print("Dostępne jednostki wagi")
     print("1. Miliglram (mg)")
     print("2. Gram (g)")
@@ -10,8 +10,30 @@ def weightcalc(): # weight calculations
     print("5. Tona (T)")
     print("6. Uncja (oz.)")
     print("7. Funty (lb.)")
-    intype = int(input("Wybierz typ wpisanej wartości: "))
-    outtype = int(input("Wybierz typ wypisanej wartości: "))
+    intype = int(input("Wybierz jednostkę  wpisanej wartości: "))
+    outtype = int(input("Wybierz jednostkę wartości wypisywanej : "))
+
+    match intype: # convering input value to grams
+        case 1: gvalue = value / 1000       # mg to g
+        case 2: gvalue = value              # g to g
+        case 3: gvalue = value * 10         # dag to g
+        case 4: gvalue = value * 1000       # kg to g
+        case 5: gvalue = value * 1000000    # T to g
+        case 6: gvalue = value * 28.3495231 # oz to g
+        case 7: gvalue = value * 453.59237  # lb to g
+        case _: print("Error") # wrong value handler
+
+    match outtype: # converting grams to output value
+        case 1: outvalue = gvalue * 1000        # g to mg
+        case 2: outvalue = gvalue               # g to g
+        case 3: outvalue = gvalue / 10          # g to dag
+        case 4: outvalue = gvalue / 1000        # g to kg
+        case 5: outvalue = gvalue / 1000000     # g to T
+        case 6: outvalue = gvalue / 28.3495231  # g to oz
+        case 7: outvalue = gvalue / 453.59237   # g to lb
+        case _: print("Error")# wrong value handler
+
+    print(outvalue)
 
 
 def lenghtcalc(): # lenght calculations
