908269e1d8d276da5be208d80c405717a9021a56
Bartosz Kocel <bartosz.kocel@outlook.com>
2025-01-06 18:34:00 +0100

    Started work on project

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# operating modes
+def weightcalc(): # weight calculations
+    print("Dostępne jednostki wagi")
+
+def lenghtcalc(): # lenght calculations
+    print("Dostępne jednostki odległości")
+
+# CLI
+print("Witaj w konwerterze jednostek!")
+print("Tryby pracy aplikacji:")
+print("1. Kalkulacje wagi ")
+print("2. Kalkulacje odległości")
+opmode = int(input("Wybierz tryb pracy: "))
+
+
+
